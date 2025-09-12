c164fc91121a030c1ddff9aad02932f191e76f65
KirillZazog <127005365+KirillZazog@users.noreply.github.com>
2025-09-12 15:15:12 +0300

    объявлены структуры трубы и КС, заданы свойства структур и организован простейший ввод/вывод свойств

--- ZakrevskiyKirillproject/ZakrevskiyKirillproject.cpp
+++ ZakrevskiyKirillproject/ZakrevskiyKirillproject.cpp
@@ -1,9 +1,32 @@
-﻿#include <iostream>
+﻿#include <fstream>
+#include <iostream>
+
 using namespace std;
+
+struct Pipe
+{
+	string name;
+	float lenth;
+	int diametr;
+	bool status;
+};
+
+struct CS
+{
+	string name;
+	int num_work;
+	int num_work_online;
+	string class_cs;
+};
 
 int main()
 {
-    int input_data;
-    cout << "Hello World!\n";
-    cin >> input_data;
+	Pipe truba;
+	CS comp_station;
+	cout << "Введите название трубы: ";
+	cin >> truba.name;
+	cout << "Введите название КС: ";
+	cin >> comp_station.name;
+	cout << "труба " << truba.name << "\nКС " << comp_station.name;
+	return 1;
 }
