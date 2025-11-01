7bdef48c1127573e2b8bc1fa8a1f848d9c788e7f
KirillZazog <127005365+KirillZazog@users.noreply.github.com>
2025-11-02 01:51:18 +0300

    Улучшен визуал интерфейса, усовершенствован ввод, добавлены проверки

--- ZakrevskiyKirillproject/ZakrevskiyKirillproject.cpp
+++ ZakrevskiyKirillproject/ZakrevskiyKirillproject.cpp
@@ -1,112 +1,282 @@
 ﻿#include <fstream>
 #include <iostream>
 #include <string>
+#include <limits>
 
 using namespace std;
 
 struct Pipe
 {
 	string name;
-	float length=0.0f;
-	int diameter=0;
-	bool status=false;
+	float length = 0.0f;
+	int diameter = 0;
+	bool status = false;
 };
 
 struct CS
 {
 	string name;
-	int num_work=0;
-	int num_work_online=0;
+	int num_work = 0;
+	int num_work_online = 0;
 	string class_cs;
 };
 
-void clear_stdin() {
-	cin.clear();
-	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+float getValidFloat(const string& prompt,
+	float min = -numeric_limits<float>::infinity(),
+	float max = numeric_limits<float>::infinity()) {
+	string input;
+	float value;
+
+	cout << prompt;
+
+	while (true) {
+		getline(cin, input);
+
+		size_t start = input.find_first_not_of(" \t");
+		size_t end = input.find_last_not_of(" \t");
+
+		if (start == string::npos) {
+			cout << "Ошибка: пустой ввод. Попробуйте снова: ";
+			continue;
+		}
+
+		input = input.substr(start, end - start + 1);
+
+		try {
+			size_t pos = 0;
+			value = stof(input, &pos);
+
+			if (pos != input.length()) {
+				cout << "Ошибка: введите корректное число. Попробуйте снова: ";
+				continue;
+			}
+
+			if (value < min || value > max) {
+				if (min == -numeric_limits<float>::infinity()) {
+					cout << "Ошибка: число должно быть <= " << max << ". Попробуйте снова: ";
+				}
+				else if (max == numeric_limits<float>::infinity()) {
+					cout << "Ошибка: число должно быть >= " << min << ". Попробуйте снова: ";
+				}
+				else {
+					cout << "Ошибка: число должно быть в диапазоне [" << min << ", " << max << "]. Попробуйте снова: ";
+				}
+				continue;
+			}
+
+			return value;
+		}
+		catch (const invalid_argument&) {
+			cout << "Ошибка: введите корректное число. Попробуйте снова: ";
+		}
+		catch (const out_of_range&) {
+			cout << "Ошибка: число слишком большое. Попробуйте снова: ";
+		}
+	}
 }
 
-void AddPipe(Pipe &t) {
-	cout << "\nВведите название трубы:";
-	clear_stdin();
-	getline(cin, t.name);
+int getValidInt(const string& prompt,
+	int min = numeric_limits<int>::min(),
+	int max = numeric_limits<int>::max()) {
+	string input;
+	int value;
 
-	cout << "Введите длину трубы (км, положительное число):";
-	while (!(cin >> t.length) || t.length < 0.0f) {
-		cout << "Неверный ввод. Введите положительное число для длины: ";
-		clear_stdin();
-	}
+	cout << prompt;
 
-	cout << "Введите диаметр трубы (м, положительное число):";
-	while (!(cin >> t.diameter) || t.diameter <= 0) {
-		cout << "Неверный ввод. Введите положительное целое для диаметра: ";
-		clear_stdin();
+	while (true) {
+		getline(cin, input);
+
+		size_t start = input.find_first_not_of(" \t");
+		size_t end = input.find_last_not_of(" \t");
+
+		if (start == string::npos) {
+			cout << "Ошибка: пустой ввод. Попробуйте снова: ";
+			continue;
+		}
+
+		input = input.substr(start, end - start + 1);
+
+		try {
+			size_t pos = 0;
+			value = stoi(input, &pos);
+
+			if (pos != input.length()) {
+				cout << "Ошибка: введите корректное целое число. Попробуйте снова: ";
+				continue;
+			}
+
+			if (value < min || value > max) {
+				if (min == numeric_limits<int>::min()) {
+					cout << "Ошибка: число должно быть <= " << max << ". Попробуйте снова: ";
+				}
+				else if (max == numeric_limits<int>::max()) {
+					cout << "Ошибка: число должно быть >= " << min << ". Попробуйте снова: ";
+				}
+				else {
+					cout << "Ошибка: число должно быть в диапазоне [" << min << ", " << max << "]. Попробуйте снова: ";
+				}
+				continue;
+			}
+
+			return value;
+		}
+		catch (const invalid_argument&) {
+			cout << "Ошибка: введите корректное целое число. Попробуйте снова: ";
+		}
+		catch (const out_of_range&) {
+			cout << "Ошибка: число слишком большое. Попробуйте снова: ";
+		}
 	}
-	cout << "Введите статус трубы (0-не в ремонте/1-в ремонте):";
-	int temp_status;
-	while (!(cin >> temp_status) || (temp_status != 0 && temp_status != 1)) {
-		cout << "Неверный ввод. Введите 0 или 1: ";
-		clear_stdin();
-	}
-	t.status = (temp_status == 1);
-	clear_stdin();
 }
 
-void AddCS(CS &c) {
-	cout << "\nВведите название КС:";
-	clear_stdin();
-	getline(cin, c.name);
-	cout << "Введите количество цехов (целое >= 0):";
-	while (!(cin >> c.num_work) || c.num_work < 0) {
-		cout << "Неверный ввод. Введите целое число >= 0:";
-		clear_stdin();
+string getValidString(const string& prompt,
+	bool allowEmpty = false) {
+	string input;
+
+	cout << prompt;
+
+	while (true) {
+		getline(cin, input);
+
+		size_t start = input.find_first_not_of(" \t");
+		size_t end = input.find_last_not_of(" \t");
+
+		if (start == string::npos) {
+			if (allowEmpty) {
+				return "";
+			}
+			cout << "Ошибка: пустая строка недопустима. Попробуйте снова: ";
+			continue;
+		}
+
+		input = input.substr(start, end - start + 1);
+		return input;
 	}
-	cout << "Введите количество рабочих цехов (целое, 0..n):";
-	while (!(cin >> c.num_work_online) || c.num_work_online < 0 || c.num_work_online > c.num_work) {
-		cout << "Неверный ввод. Введите целое в диапазоне 0.." << c.num_work << ";";
-		clear_stdin();
-	}
-	clear_stdin();
-	cout << "Введите класс станциии:";
-	getline(cin, c.class_cs);
 }
 
-void ShowAll(Pipe &t, CS &c) {
-	cout << "\nНазвание трубы: " << t.name << endl
-		<< "Длина трубы: " << t.length << endl
-		<< "Диаметр трубы: " << t.diameter << endl
-		<< "Статус трубы (0-не в ремонте/1-в ремонте): " << t.status << endl
-		<< "\nНазвание КС: " << c.name << endl
-		<< "Количество цехов: " << c.num_work << endl
-		<< "Количество рабочих цехов: " << c.num_work_online << endl
-		<< "Класс станции: " << c.class_cs << endl;
-}
+bool getValidBool(const string& prompt) {
+	string input;
 
-void EditPipe(Pipe &t) {
-	cout << "Текущий статус трубы: " << (t.status ? "в ремонте" : "не в ремонте") << endl;
-	cout << "Введите новый статус трубы (0-не в ремонте/1-в ремонте): ";
-	int temp_status;
-	while (!(cin >> temp_status) || (temp_status != 0 && temp_status != 1)) {
-		cout << "Неверный ввод. Введите 0 или 1: ";
-		clear_stdin();
+	cout << prompt << " (0-нет/1-да): ";
+
+	while (true) {
+		getline(cin, input);
+
+		size_t start = input.find_first_not_of(" \t");
+		size_t end = input.find_last_not_of(" \t");
+
+		if (start == string::npos) {
+			cout << "Ошибка: пустой ввод. Попробуйте снова: ";
+			continue;
+		}
+
+		input = input.substr(start, end - start + 1);
+
+		for (char& c : input) {
+			c = tolower(c);
+		}
+
+		if (input == "да" || input == "yes" || input == "y" ||
+			input == "1" || input == "true" || input == "т") {
+			return true;
+		}
+
+		if (input == "нет" || input == "no" || input == "n" ||
+			input == "0" || input == "false" || input == "н") {
+			return false;
+		}
+
+		cout << "Ошибка: введите 0 или 1. Попробуйте снова: ";
 	}
-	t.status = (temp_status == 1);
-	clear_stdin();
 }
 
-void EditCS(CS &c) {
+void AddPipe(Pipe& t) {
+	cout << "\n=== ДОБАВЛЕНИЕ ТРУБЫ ===\n";
+
+	t.name = getValidString("Введите название трубы: ");
+	t.length = getValidFloat("Введите длину трубы (км, положительное число): ", 0.0f);
+	t.diameter = getValidInt("Введите диаметр трубы (мм, положительное число): ", 1);
+	t.status = getValidBool("Статус трубы (0-не в ремонте/1-в ремонте)");
+
+	cout << "Труба успешно добавлена!\n";
+}
+
+void AddCS(CS& c) {
+	cout << "\n=== ДОБАВЛЕНИЕ КОМПРЕССОРНОЙ СТАНЦИИ ===\n";
+
+	c.name = getValidString("Введите название КС: ");
+	c.num_work = getValidInt("Введите количество цехов (целое >= 0): ", 0);
+	c.num_work_online = getValidInt("Введите количество рабочих цехов (0.." + to_string(c.num_work) + "): ", 0, c.num_work);
+	c.class_cs = getValidString("Введите класс станции: ");
+
+	cout << "Компрессорная станция успешно добавлена!\n";
+}
+
+void ShowAll(const Pipe& t, const CS& c) {
+	cout << "\n=== ВСЕ ОБЪЕКТЫ ===\n";
+
+	if (t.name.empty() && c.name.empty()) {
+		cout << "Нет данных для отображения. Добавьте трубу или КС.\n";
+		return;
+	}
+
+	if (!t.name.empty()) {
+		cout << "\n--- ТРУБА ---\n";
+		cout << "Название: " << t.name << endl;
+		cout << "Длина: " << t.length << " км" << endl;
+		cout << "Диаметр: " << t.diameter << " мм" << endl;
+		cout << "Статус: " << (t.status ? "в ремонте" : "не в ремонте") << endl;
+	}
+
+	if (!c.name.empty()) {
+		cout << "\n--- КОМПРЕССОРНАЯ СТАНЦИЯ ---\n";
+		cout << "Название: " << c.name << endl;
+		cout << "Количество цехов: " << c.num_work << endl;
+		cout << "Количество рабочих цехов: " << c.num_work_online << endl;
+		cout << "Класс станции: " << c.class_cs << endl;
+	}
+}
+
+void EditPipe(Pipe& t) {
+	if (t.name.empty()) {
+		cout << "\nОшибка: труба не создана. Сначала добавьте трубу (опция 1).\n";
+		return;
+	}
+
+	cout << "\n=== РЕДАКТИРОВАНИЕ ТРУБЫ ===\n";
+	cout << "Текущий статус: " << (t.status ? "в ремонте" : "не в ремонте") << endl;
+
+	t.status = getValidBool("Введите новый статус (0-не в ремонте/1-в ремонте)");
+
+	cout << "Статус трубы обновлён!\n";
+}
+
+void EditCS(CS& c) {
+	if (c.name.empty()) {
+		cout << "\nОшибка: КС не создана. Сначала добавьте КС (опция 2).\n";
+		return;
+	}
+
+	cout << "\n=== РЕДАКТИРОВАНИЕ КС ===\n";
 	cout << "Текущее количество рабочих цехов: " << c.num_work_online << " из " << c.num_work << endl;
-	cout << "Чтобы запустить цех, введите: 1\nЧтобы остановить цех, введите: 0\nВаш выбор: ";
-	int opt;
-	while (!(cin >> opt) || (opt != 0 && opt != 1)) {
-		cout << "Неверный ввод. Введите 1 для запуска, 0 для остановки: ";
-		clear_stdin();
+
+	if (c.num_work == 0) {
+		cout << "Невозможно изменить количество рабочих цехов: всего цехов = 0.\n";
+		return;
 	}
+
+	cout << "1. Запустить цех\n";
+	cout << "0. Остановить цех\n";
+	int opt = getValidInt("Ваш выбор: ", 0, 1);
+
 	if (opt == 1) {
 		if (c.num_work_online < c.num_work) {
 			c.num_work_online++;
 			cout << "Цех запущен. Рабочих цехов: " << c.num_work_online << "\n";
 		}
-		else { cout << "Все цеха уже в работе, запуск невозможен.\n"; }
+		else {
+			cout << "Все цеха уже в работе, запуск невозможен.\n";
+		}
 	}
 	else {
 		if (c.num_work_online > 0) {
@@ -117,12 +287,22 @@ void EditCS(CS &c) {
 			cout << "Нет рабочих цехов для остановки.\n";
 		}
 	}
-	clear_stdin();
 }
 
 int Save(const Pipe& t, const CS& c) {
+	cout << "\n=== СОХРАНЕНИЕ ДАННЫХ ===\n";
+
+	if (t.name.empty() && c.name.empty()) {
+		cout << "Нет данных для сохранения.\n";
+		return 1;
+	}
+
 	ofstream fout("data.txt");
-	if (!fout) { cerr << "Ошибка при создании/перезаписи файла data.txt!\n"; return 1; }
+	if (!fout) {
+		cerr << "Ошибка при создании/перезаписи файла data.txt!\n";
+		return 1;
+	}
+
 	fout << "Труба:\n"
 		<< t.name << '\n'
 		<< t.length << '\n'
@@ -133,12 +313,15 @@ int Save(const Pipe& t, const CS& c) {
 		<< c.num_work << '\n'
 		<< c.num_work_online << '\n'
 		<< c.class_cs << '\n';
+
 	fout.close();
 	cout << "Данные успешно сохранены в файл data.txt!\n";
 	return 0;
 }
 
 int Download(Pipe& t, CS& c) {
+	cout << "\n=== ЗАГРУЗКА ДАННЫХ ===\n";
+
 	ifstream fin("data.txt");
 	if (!fin) {
 		cerr << "Ошибка при открытии файла data.txt (файл не найден?)\n";
@@ -204,24 +387,24 @@ int Download(Pipe& t, CS& c) {
 	return 0;
 }
 
-void Show_menu(Pipe &t, CS &c) {
+void Show_menu(Pipe& t, CS& c) {
 	int option = 0;
 	while (true) {
-		cout << "\nВыберите опцию:\n"
-			<< "1. Добавить трубу\n"
-			<< "2. Добавить КС\n"
-			<< "3. Посмотреть все объекты\n"
-			<< "4. Редактировать трубу\n"
-			<< "5. Редактировать КС\n"
-			<< "6. Сохранить\n"
-			<< "7. Загрузить\n"
-			<< "0. Выход\n"
-			<< "Ваш выбор: ";
-		if (!(cin >> option)) {
-			cout << "Неверный ввод. Введите число от 0 до 7.\n";
-			clear_stdin();
-			continue;
-		}
+		cout << "\n========================================\n";
+		cout << "           ГЛАВНОЕ МЕНЮ\n";
+		cout << "========================================\n";
+		cout << "1. Добавить трубу\n";
+		cout << "2. Добавить КС\n";
+		cout << "3. Посмотреть все объекты\n";
+		cout << "4. Редактировать трубу\n";
+		cout << "5. Редактировать КС\n";
+		cout << "6. Сохранить\n";
+		cout << "7. Загрузить\n";
+		cout << "0. Выход\n";
+		cout << "========================================\n";
+
+		option = getValidInt("Ваш выбор: ", 0, 7);
+
 		switch (option) {
 		case 1: AddPipe(t); break;
 		case 2: AddCS(c); break;
@@ -230,8 +413,11 @@ void Show_menu(Pipe &t, CS &c) {
 		case 5: EditCS(c); break;
 		case 6: Save(t, c); break;
 		case 7: Download(t, c); break;
-		case 0: return;
-		default: cout << "\nВыбрана несуществующая опция, введите число от 0 до 7!\n\n";
+		case 0:
+			cout << "\nВыход из программы. До свидания!\n";
+			return;
+		default:
+			cout << "\nНепредвиденная ошибка!\n";
 		}
 	}
 }
